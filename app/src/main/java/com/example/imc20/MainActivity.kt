cd30a0a2556f00f6aeba38737a6c04c9989347d1
THIAGO JOSÉ BUENO OLIVEIRA <21194115@CFP127ALUNO.LOCAL>
2021-09-14 17:02:34 -0300

    atualizacao

--- app/src/main/java/com/example/imc20/MainActivity.kt
+++ app/src/main/java/com/example/imc20/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.imc20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editTextPeso = findViewById<EditText>(R.id.et_peso)
+        val editTextAltura = findViewById<EditText>(R.id.et_altura)
+        val  buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        val textViewResultado = findViewById<TextView>(R.id.tv_resultado)
+
+
+       buttonCalcular.setOnClickListener {
+           var peso = editTextPeso.text.toString().toInt()
+           var altura = editTextAltura.text.toString().toDouble()
+
+           var imc = peso / Math.pow(altura, 2.0)
+
+           textViewResultado.text = imc.toString()
+       }
+
+
     }
 }
